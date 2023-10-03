b51cb5dc17476a2b7486a0ec7a03ccaf2feb0a51
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-10-03 15:37:46 +0200

    Task day 2 completed

--- src/main/java/org/java/app/controller/MainController.java
+++ src/main/java/org/java/app/controller/MainController.java
@@ -11,93 +11,82 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
 public class MainController {
 
-	
-	
 	@GetMapping("/")
 	@ResponseBody
 	public String test() {
 		return "ciao mondo";
 	}
-	
+
 	@GetMapping("/welcome")
-	
+
 	public String welcome() {
-		
+
 		return "index";
 	}
-	
+
 	@GetMapping("/movies")
 	public String getBestMovies(Model model) {
 		List<Movie> bestMovies = getBestMovies();
-			
-		
+
 		model.addAttribute("bestMovies", bestMovies);
-        return "movies";
-		
+		return "movies";
+
 	}
-    @GetMapping("/movies/{id}")
-    @ResponseBody
-    public String getMovieTitleById(@PathVariable int id) {
-        List<Movie> movies = getBestMovies(); 
 
-        for (Movie movie : movies) {
-            if (movie.getId() == id) {
-                return movie.getTitolo(); 
-            }
-        }
+	@GetMapping("/movies/{id}")
+	public String getMovieTitleById(@PathVariable int id, Model model) {
+		List<Movie> movies = getBestMovies();
 
+		for (Movie movie : movies) {
+			if (movie.getId() == id) {
+				model.addAttribute("movie", movie);
+				return "movie_detail"; 
+			}
+		}
 
-        return "Film non trovato";
-    }
-    
-    @GetMapping("/songs/{id}")
-    @ResponseBody
-    public String getSongTitleById(@PathVariable int id) {
-        List<Song> songs = getBestSongs(); 
+		return "film_not_found"; 
+	}
 
-        for (Song song : songs) {
-            if (song.getId() == id) {
-                return song.getTitolo(); 
-            }
-        }
+	@GetMapping("/songs/{id}")
+	public String getSongTitleById(@PathVariable int id, Model model) {
+		List<Song> songs = getBestSongs();
 
+		for (Song song : songs) {
+			if (song.getId() == id) {
+				model.addAttribute("song", song);
+				return "song_detail"; 
+			}
+		}
 
-        return "Canzone non trovata";
-    }
-
-
+		return "song_not_found"; 
+	}
 
 	@GetMapping("/songs")
 	public String getBestSongs(Model model) {
-	    List<Song> bestSongs = getBestSongs();
-	    model.addAttribute("bestSongs", bestSongs);
-	    return "songs";
+		List<Song> bestSongs = getBestSongs();
+		model.addAttribute("bestSongs", bestSongs);
+		return "songs";
 	}
 
-	
-	
-	 private List<Movie> getBestMovies() {
-	        List<Movie> movies = new ArrayList<>();
-	        movies.add(new Movie(1, "Ritorno al futuro"));
-	        movies.add(new Movie(2, "Indiana Jones"));
-	        movies.add(new Movie(3, "Jurassic park"));
-	        
-	        return movies;
-	    }
+	private List<Movie> getBestMovies() {
+		List<Movie> movies = new ArrayList<>();
+		movies.add(new Movie(1, "Ritorno al futuro"));
+		movies.add(new Movie(2, "Indiana Jones"));
+		movies.add(new Movie(3, "Jurassic park"));
 
-	    private List<Song> getBestSongs() {
-	        List<Song> songs = new ArrayList<>();
-	        songs.add(new Song(1, "Back in black"));
-	        songs.add(new Song(2, "You give love a bad name"));
-	        songs.add(new Song(3, "Bohemian Rapsody"));
-	
-	        return songs;
-	    }
-	    
-	    
-	
+		return movies;
+	}
+
+	private List<Song> getBestSongs() {
+		List<Song> songs = new ArrayList<>();
+		songs.add(new Song(1, "Back in black"));
+		songs.add(new Song(2, "You give love a bad name"));
+		songs.add(new Song(3, "Bohemian Rapsody"));
+
+		return songs;
+	}
+
 }
